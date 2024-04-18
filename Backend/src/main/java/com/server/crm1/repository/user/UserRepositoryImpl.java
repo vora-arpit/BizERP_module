8a80dffb68a2553f4ae9b922bb0f6d40cb63ceb6
arpit_vora <arpitvora09@gmail.com>
2024-04-18 11:37:17 +0530

    remove error of userpage in backend userRepositoryImpl

--- Backend/src/main/java/com/server/crm1/repository/user/UserRepositoryImpl.java
+++ Backend/src/main/java/com/server/crm1/repository/user/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.server.crm1.repository.user;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,37 +17,42 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 	private NamedParameterJdbcTemplate jdbc;
 
 	@Override
-	public List<User> search(String filter) {
-		String filterParam = StringUtils.isBlank(filter) ? "" : " and (x.name ilike :text or email ilike ':text') ";
-		String sql = "select \n" +
-				"	x.id ,\n" +
-				"	x.name ,\n" +
-				"	x.email ,\n" +
-				"	x.image_Url,\n" +
-				"	STRING_AGG (r.name,', ') roles \n" +
-				" from users x \n" +
-				" left outer join user_role ru on ru.user_id=x.id \n" +
-				" left outer join role r on ru.role_name=r.name \n" +
-				" where true \n" +
-				filterParam +
-				" group by x.id \n" +
-				" order by x.name \n";
-		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("text", "%" + filter + "%");
-		return jdbc.query(
-				sql,
-				mapSqlParameterSource,
-				(rs, rowNum) -> {
-					int idx = 1;
-					User user = new User();
-					user.setId(rs.getInt(idx++));
-					user.setName(rs.getString(idx++));
-					user.setEmail(rs.getString(idx++));
-					user.setImageUrl(rs.getString(idx++));
-					String rolesSTR = rs.getString(idx++);
-					user.setRoles(Arrays.asList(rolesSTR.split(", ")));
-					return user;
-				});
-	}
+public List<User> search(String filter) {
+    String filterParam = StringUtils.isBlank(filter) ? "" : " and (x.name ilike :text or email ilike ':text') ";
+    String sql = "select \n" +
+            "   x.id ,\n" +
+            "   x.name ,\n" +
+            "   x.email ,\n" +
+            "   x.image_Url,\n" +
+            "   STRING_AGG (r.name,', ') roles \n" +
+            " from users x \n" +
+            " left outer join user_role ru on ru.user_id=x.id \n" +
+            " left outer join role r on ru.role_name=r.name \n" +
+            " where true \n" +
+            filterParam +
+            " group by x.id \n" +
+            " order by x.name \n";
+    MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("text", "%" + filter + "%");
+    return jdbc.query(
+            sql,
+            mapSqlParameterSource,
+            (rs, rowNum) -> {
+                int idx = 1;
+                User user = new User();
+                user.setId(rs.getInt(idx++));
+                user.setName(rs.getString(idx++));
+                user.setEmail(rs.getString(idx++));
+                user.setImageUrl(rs.getString(idx++));
+                String rolesSTR = rs.getString(idx++);
+                if (rolesSTR != null) {
+                    user.setRoles(Arrays.asList(rolesSTR.split(", ")));
+                } else {
+                    user.setRoles(Collections.emptyList()); // Set empty list if rolesSTR is null
+                }
+                return user;
+            });
+}
+
 
 }
