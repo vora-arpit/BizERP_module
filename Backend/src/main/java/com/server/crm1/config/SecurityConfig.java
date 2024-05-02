3bf6aaed3512af7bcd8cc63a5f8d227013b06f6d
arpit_vora <arpitvora09@gmail.com>
2024-05-02 14:41:30 +0530

    Integrated Grafana and Promethues with server

--- Backend/src/main/java/com/server/crm1/config/SecurityConfig.java
+++ Backend/src/main/java/com/server/crm1/config/SecurityConfig.java
@@ -110,7 +110,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						"/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg",
 						"/**/*.html", "/**/*.css", "/**/*.js")
 				.permitAll()
-				.antMatchers("/auth/**", "/oauth2/**","/payment/success","/invoice/generate-invoice")
+				.antMatchers("/auth/**", "/oauth2/**","/payment/success","/invoice/generate-invoice","/actuator/prometheus")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
